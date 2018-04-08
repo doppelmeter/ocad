84bcf6dbd68e36393bb44376d85a9d99e564f962
huema-ch <38141610+huema-ch@users.noreply.github.com>
2018-04-08 17:48:30 +0200

    initial setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='ocad',
+    version='0.0.1',
+    author='Marius Huerzeler',
+    author_email='huerzeler.marius@gmail.com',
+    packages=['ocad', 'ocad.test'],
+    url='',
+    license='MIT',
+    description='Module for handling ocad-files',
+    long_description=open('README.md').read(),
+    python_requires='>=3',
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          ],
+
+)
